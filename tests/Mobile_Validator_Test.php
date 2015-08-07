3fe4707cd340f9c0084ac0a87a37aa1038d99ff2
southp <southp0105@gmail.com>
2015-08-07 18:06:08 +0800

    Create a commonly-used $mobile_validator across all tests in the fixture.

--- tests/Mobile_Validator_Test.php
+++ tests/Mobile_Validator_Test.php
@@ -3,9 +3,13 @@
 namespace Automattic\Phone;
 
 class Mobile_Validator_Test extends \PHPUnit_Framework_TestCase {
-	public function testNormalize() {
-		$mobile_validator = new Mobile_Validator();
+	private $mobile_validator;
 
+	protected function setUp() {
+		$this->mobile_validator = new Mobile_Validator();
+	}
+
+	public function test_input_parameter_1() {
 		// Intentionally failing.
 		$this->assertFalse(true);
 	}
