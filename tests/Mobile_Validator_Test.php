9f5f8f000a7f65d7ee96a0929e111db61a551766
southp <southp0105@gmail.com>
2015-08-10 22:35:04 +0800

    Migrate the 'Testing input parameter Phone' tests.

--- tests/Mobile_Validator_Test.php
+++ tests/Mobile_Validator_Test.php
@@ -10,7 +10,30 @@ class Mobile_Validator_Test extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function test_input_parameter_1() {
-		// Intentionally failing.
-		$this->assertFalse(true);
+		$number = '(852) 569-8900';
+		$result = $this->mobile_validator->normalize( $number );
+		$this->assertEmpty( $result );
+	}
+
+	public function test_input_parameter_2() {
+		$number = '+1 (817) 569-8900';
+		$result = array( '+18175698900', 'USA' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number ), $result );
+	}
+
+	public function test_input_parameter_3() {
+		$number = '+852 6569-8900';
+		$result = array( '+85265698900', 'HKG' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number ), $result );
+	}
+
+	public function test_input_parameter_4() {
+		$number = '+852 6569-8900';
+		$country = 'HKG';
+		$result = ['+85265698900', 'HKG'];
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
 	}
 }
