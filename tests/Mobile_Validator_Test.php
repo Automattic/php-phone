0e97e558bc5e1359225f2005cb64c5f5a39527be
southp <southp0105@gmail.com>
2015-08-10 23:12:34 +0800

    Migrate the "Testing MEX Phone" tests.

--- tests/Mobile_Validator_Test.php
+++ tests/Mobile_Validator_Test.php
@@ -140,4 +140,123 @@ class Mobile_Validator_Test extends \PHPUnit_Framework_TestCase {
 
 		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
 	}
+
+	public function test_MEX_phone_1() {
+		$number = '+52 1 762 100 9517';
+		$country = null;
+		$result = array( '+5217621009517', 'MEX' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_MEX_phone_2() {
+		$number = '+52 1 762 100 9517';
+		$country = 'MEX';
+		$result = array( '+5217621009517', 'MEX' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_MEX_phone_3() {
+		$number = '+52 1 762 100 9517';
+		$country = 'USA';
+		$result = array();
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_MEX_phone_4() {
+		$number = '+52 1 762 100 9517';
+		$country = 'Mexico';
+		$result = array( '+5217621009517', 'MEX' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_MEX_phone_5() {
+		$number = '+52 1 762 100 9517';
+		$country = 'United States';
+		$result = array();
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_MEX_phone_6() {
+		$number = '+52 62 100 9517';
+		$country = null;
+		$result = array();
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_MEX_phone_7() {
+		$number = '+52 62 100 9517';
+		$country = 'MEX';
+		$result = array();
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_MEX_phone_8() {
+		$number = '+52 62 100 9517';
+		$country = 'USA';
+		$result = array();
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_MEX_phone_9() {
+		$number = '+52 62 100 9517';
+		$country = 'Mexico';
+		$result = array();
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_MEX_phone_10() {
+		$number = '+52 62 100 9517';
+		$country = 'United States';
+		$result = array();
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_MEX_phone_11() {
+		$number = '52762 100 9517';
+		$country = null;
+		$result = array();
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_MEX_phone_12() {
+		$number = '762 100 9517';
+		$country = 'MEX';
+		$result = array( '+527621009517', 'MEX' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_MEX_phone_13() {
+		$number = '762 100 9517';
+		$country = 'MEXINVALID';
+		$result = array();
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_MEX_phone_14() {
+		$number = '762 100 9517';
+		$country = 'Mexico';
+		$result = array( '+527621009517', 'MEX' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+	public function test_MEX_phone_15() {
+		$number = '762 100 9517';
+		$country = 'Mexico Invalid';
+		$result = array();
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
 }
