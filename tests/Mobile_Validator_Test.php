8c9a81b6d06a842b98394dc46029369d93b71f3a
southp <southp0105@gmail.com>
2015-08-10 22:47:55 +0800

    Migrate the "Testing USA Phone" tests.

--- tests/Mobile_Validator_Test.php
+++ tests/Mobile_Validator_Test.php
@@ -32,7 +32,111 @@ class Mobile_Validator_Test extends \PHPUnit_Framework_TestCase {
 	public function test_input_parameter_4() {
 		$number = '+852 6569-8900';
 		$country = 'HKG';
-		$result = ['+85265698900', 'HKG'];
+		$result = array( '+85265698900', 'HKG' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_USA_phone_1() {
+		$number = '(852) 569-8900';
+		$country = '';
+		$result = array();
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_USA_phone_2() {
+		$number = '+1 (817) 569-8900';
+		$country = '';
+		$result = array( '+18175698900', 'USA' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_USA_phone_3() {
+		$number = '+1 (817) 569-8900';
+		$country = null;
+		$result = array( '+18175698900', 'USA' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_USA_phone_4() {
+		$number = '2121234567';
+		$country = '';
+		$result = array( '+12121234567', 'USA' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_USA_phone_5() {
+		$number = '22-6569-8900';
+		$country = '';
+		$result = array();
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_USA_phone_6() {
+		$number = '22-5569-8900';
+		$country = '';
+		$result = array( '+12255698900', 'USA' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_USA_phone_7() {
+		$number = '+1 (817) 569-8900';
+		$country = 'United States';
+		$result = array( '+18175698900', 'USA' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_USA_phone_8() {
+		$number = '+1 (817) 569-8900';
+		$country = 'United States ';
+		$result = array ( '+18175698900', 'USA' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_USA_phone_9() {
+		$number = '+1 (817) 569-8900';
+		$country = 'USA';
+		$result = array( '+18175698900', 'USA' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_USA_phone_10() {
+		$number = '+1 (817) 569-8900';
+		$country = 'USA ';
+		$result = array( '+18175698900', 'USA' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_USA_phone_11() {
+		$number = '+1 (817) 569-8900';
+		$country = 'US';
+		$result = array( '+18175698900', 'USA' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_USA_phone_12() {
+		$number = '+1 (817) 569-8900';
+		$country = ' US';
+		$result = array( '+18175698900', 'USA' );
+
+		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
+	}
+
+	public function test_USA_phone_13() {
+		$number = '+1 (817) 569-8900';
+		$country = 'HKG';
+		$result = array();
 
 		$this->assertEquals( $this->mobile_validator->normalize( $number, $country ), $result );
 	}
