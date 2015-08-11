8197d392f9716082173716b14e1ebade58710867
southp <southp0105@gmail.com>
2015-08-12 00:10:52 +0800

    Migrate get_iso3166 of node-phone as get_iso3166_entry.
    get_iso3166 is a little vague for me, as I don't know whether it is
    the whole table of iso3166 data or just an entry. So I append "_entry"
    for clarification.

--- src/Mobile_Validator.php
+++ src/Mobile_Validator.php
@@ -5,8 +5,34 @@ namespace Automattic\Phone;
 require_once( __DIR__ . '/Iso3166.php' );
 
 class Mobile_Validator {
-	private function get_iso3166_entry( $country ) {
+	private function get_iso3166_entry( $country_name ) {
+		switch ( strlen( $country_name ) ) {
+		case 0:
+			return Iso3166::$data[0];
 
+		case 2:
+			foreach ( Iso3166::$data as $iso3166_entry ){
+				if ( strtoupper( $country_name ) == $iso3166_entry["alpha2"] ) {
+					return $iso3166_entry;
+				}
+			}
+
+		case 3:
+			foreach ( Iso3166::$data as $iso3166_entry ) {
+				if ( strtoupper( $country_name ) == $iso3166_entry["alpha3"] ) {
+					return $iso3166_entry;
+				}
+			}
+
+		default:
+			foreach ( Iso3166::$data as $iso3166_entry ) {
+				if (strtoupper( $country_name ) == strtoupper( $iso3166_entry["country_name"] ) ) {
+					return $iso3166_entry;
+				}
+			}
+		}
+
+		return null;
 	}
 
 	private function get_iso3166_by_phone( $phone ) {
