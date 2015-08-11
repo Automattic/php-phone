8a51c13ec513ed58edb3dfdd9ed946556d6cf500
southp <southp0105@gmail.com>
2015-08-11 23:56:30 +0800

    Preparing the required functions' stubs before migration.

--- src/Mobile_Validator.php
+++ src/Mobile_Validator.php
@@ -5,7 +5,18 @@ namespace Automattic\Phone;
 require_once( __DIR__ . '/Iso3166.php' );
 
 class Mobile_Validator {
-	function normalize( $phone_number ) {
+	private function get_iso3166_entry( $country ) {
+
+	}
+
+	private function get_iso3166_by_phone( $phone ) {
+	}
+
+	private function validate_phone_iso3166( $phone, $iso3166_entry ) {
+	}
+
+	function normalize( $phone_number, $country ) {
+
 		return array( "", "" );
 	}
 }
